2154e0ba03f97b809d9e480d67456d468a916ea7
Obrige <174951221+obrige@users.noreply.github.com>
2026-06-07 23:31:10 +0800

    Warm up WebView engine before cleaning

--- app/src/main/java/com/aurora/lens/LensApp.kt
+++ app/src/main/java/com/aurora/lens/LensApp.kt
@@ -2,15 +2,18 @@ package com.aurora.lens
 
 import android.app.Application
 import android.util.Log
+import android.webkit.CookieManager
 import com.aurora.lens.util.PrivacyCleaner
 
 class LensApp : Application() {
     override fun onCreate() {
         super.onCreate()
         try {
+            // Warm up WebView engine before cleaning to avoid crashes on some devices
+            CookieManager.getInstance()
             PrivacyCleaner.aggressiveClean(this)
-        } catch (e: Exception) {
-            Log.e("LensApp", "PrivacyCleaner failed", e)
+        } catch (t: Throwable) {
+            Log.e("LensApp", "PrivacyCleaner failed", t)
         }
     }
 }
