4512796ac9db38f2c5f9083c9d2ce3528feb3783
Obrige <174951221+obrige@users.noreply.github.com>
2026-06-07 21:24:26 +0800

    Add try-catch to LensApp to prevent crash on startup

--- app/src/main/java/com/aurora/lens/LensApp.kt
+++ app/src/main/java/com/aurora/lens/LensApp.kt
@@ -1,11 +1,16 @@
 package com.aurora.lens
 
 import android.app.Application
+import android.util.Log
 import com.aurora.lens.util.PrivacyCleaner
 
 class LensApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        PrivacyCleaner.aggressiveClean(this)
+        try {
+            PrivacyCleaner.aggressiveClean(this)
+        } catch (e: Exception) {
+            Log.e("LensApp", "PrivacyCleaner failed", e)
+        }
     }
 }
